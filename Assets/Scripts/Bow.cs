f747c4910a9e62e35403cf22d783b28da9dd1cc3
VladoZir <vladimir.zir2@gmail.com>
2025-02-06 21:23:49 +0100

    bow

--- Assets/Scripts/Bow.cs
+++ Assets/Scripts/Bow.cs
@@ -6,10 +6,21 @@ public class Bow : MonoBehaviour
     public GameObject arrow;
     public float launchForce;
     public Transform shotPoint;
+
+    public GameObject point;
+    GameObject[] points;
+    public int numberOfPoints;
+    public float spaceBetweenPoints;
+    Vector2 direction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        points = new GameObject[numberOfPoints];
+        for (int i = 0; i < numberOfPoints; i++)
+        {
+            points[i] = Instantiate(point, shotPoint.position, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
@@ -17,12 +28,17 @@ public class Bow : MonoBehaviour
     {
         Vector2 bowPosition = transform.position;
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = mousePosition - bowPosition;
+        direction = mousePosition - bowPosition;
         transform.right = direction;
 
         if (Input.GetMouseButtonDown(0))
         {
             Shoot();
+        }
+
+        for (int i = 0; i < numberOfPoints; i++)
+        {
+            points[i].transform.position = PointPosition(i * spaceBetweenPoints);
         }
 
     }
@@ -32,4 +48,11 @@ public class Bow : MonoBehaviour
         GameObject newArrow = Instantiate(arrow, shotPoint.position, shotPoint.rotation);
         newArrow.GetComponent<Rigidbody2D>().linearVelocity = transform.right*launchForce;
     }
+
+    Vector2 PointPosition(float t)
+    {
+        Vector2 position = (Vector2)shotPoint.position + (direction.normalized * launchForce * t) + 0.5f * Physics2D.gravity * (t*t);
+        return position;
+    }
+
 }
