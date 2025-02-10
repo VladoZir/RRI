dacabbcf047d111ce04396b4d64e62bab302a3cc
VladoZir <vladimir.zir2@gmail.com>
2025-02-10 15:12:47 +0100

    Arrow hit sound added

--- Assets/Scripts/ArrowNew.cs
+++ Assets/Scripts/ArrowNew.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class ArrowNew : MonoBehaviour
@@ -6,6 +7,8 @@ public class ArrowNew : MonoBehaviour
     bool hasHit;
     public int damage = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public AudioSource arrowHitAudio;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -27,16 +30,34 @@ public class ArrowNew : MonoBehaviour
         rb.linearVelocity = Vector2.zero;
         rb.bodyType = RigidbodyType2D.Kinematic;
 
+        // Make the arrow invisible and disable its collider
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+
         if (collision.gameObject.CompareTag("Enemy"))
         {
+            arrowHitAudio.Play();
             IEnemy enemy = collision.gameObject.GetComponent<IEnemy>();
             if (enemy != null)
             {
                 enemy.TakeDamage(damage); // Call TakeDamage for any enemy type
             }
+
+            StartCoroutine(DestroyAfterSound()); // Wait for audio before destroying
+        }
+        else
+        {
+            Destroy(gameObject); // If not an enemy, destroy immediately
         }
 
-        Destroy(gameObject);
         Debug.Log(collision.transform.name);
     }
+
+    private IEnumerator DestroyAfterSound()
+    {
+        yield return new WaitForSeconds(arrowHitAudio.clip.length); // Wait for the audio to finish
+        Destroy(gameObject);
+    }
+
+
 }
