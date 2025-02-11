2cfafa96d43241041faec751f21e06d97c21b71e
VladoZir <vladimir.zir2@gmail.com>
2025-02-11 17:16:24 +0100

    Pause menu added

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -4,23 +4,54 @@ using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
-    public GameObject deathPanel; // Assign Game Over Panel in the Inspector
+    public GameObject deathPanel;
+    public GameObject pausePanel;
+    public GameObject pauseButton;
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
 
     public void ShowGameOverScreen()
     {
         deathPanel.SetActive(true);
-        Time.timeScale = 0f; // Pause the game
+        Time.timeScale = 0f; 
     }
 
     public void RestartGame()
     {
-        Time.timeScale = 1f; // Resume game speed
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Reload level
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
     }
 
     public void LoadMainMenu()
     {
-        Time.timeScale = 1f; // Resume game speed
-        SceneManager.LoadScene("MainMenu"); // Make sure your main menu scene is added to build settings
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene("MainMenu"); 
     }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0f;
+            pauseButton.SetActive(false);
+        }
+        else
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1f;
+            pauseButton.SetActive(true);
+        }
+    }
+
 }
