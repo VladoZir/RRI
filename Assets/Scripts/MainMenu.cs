502802b0a724aebd2e33774fcff2df8adea73838
VladoZir <vladimir.zir2@gmail.com>
2025-06-09 19:53:42 +0200

    Settings panel
    settings panel sa gumbom za mute unmute i gumbom za reset progress. mute gumb treba napravit da mute sve scene a ne samo main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,19 +1,58 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject levelSelectionPanel;
+    public GameObject settingsPanel;
+
+    public int level;
+
+    public Button[] levelButtons;
+    public Button muteButton;
+
+    private bool isToggledSelection = false;
+    private bool isToggledSettings = false;
+    private bool isMuted = false;
+
+    public AudioSource musicSource;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //PlayerPrefs.DeleteKey("unlockedLevels");
+        int unlockedLevels = PlayerPrefs.GetInt("unlockedLevels", 0);
 
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            levelButtons[i].interactable = (i < unlockedLevels);
+        }
+
+        isMuted = PlayerPrefs.GetInt("musicMuted", 0) == 1;
+        UpdateMusicState();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ToggleMusic()
+    {
+        isMuted = !isMuted;
+        PlayerPrefs.SetInt("musicMuted", isMuted ? 1 : 0);
+        UpdateMusicState();
+    }
+
+    void UpdateMusicState()
+    {
+        if (musicSource != null)
+            musicSource.mute = isMuted;
+        /*
+        if (muteButtonText != null)
+            muteButtonText.text = isMuted ? "Unmute Music" : "Mute Music";
+        */
     }
 
     public void PlayGame()
@@ -27,5 +66,49 @@ public class MainMenu : MonoBehaviour
         }
 
         SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    public void ToggleSelection()
+    {
+        isToggledSelection = !isToggledSelection;
+
+        if (isToggledSelection)
+        {
+            levelSelectionPanel.SetActive(true);
+        }
+        else
+        {
+            levelSelectionPanel.SetActive(false);
+        }
+    }
+
+    public void ToggleSettings()
+    {
+        isToggledSettings = !isToggledSettings;
+
+        if (isToggledSettings)
+        {
+            settingsPanel.SetActive(true);
+        }
+        else
+        {
+            settingsPanel.SetActive(false);
+        }
+    }
+
+    public void OpenScene(int levelNumber)
+    {
+        level = levelNumber;
+        SceneManager.LoadScene("Level" + level.ToString());
+    }
+
+    public void ResetProgress()
+    {
+        PlayerPrefs.DeleteKey("unlockedLevels");
+        // Ako imaš collectables, npr. PlayerPrefs.DeleteKey("coins");
+        // Dodaj još što trebaš resetirati
+
+        Debug.Log("Progress reset!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
     }
 }
